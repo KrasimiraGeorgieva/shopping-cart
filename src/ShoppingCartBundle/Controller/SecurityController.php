d723562f5d631eb66074a3bce74672a5a04c96cc
Krasimira Georgieva <krassi77bg@gmail.com>
2017-12-07 23:36:16 +0200

    Add SecurityController.

---
+++ src/ShoppingCartBundle/Controller/SecurityController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace ShoppingCartBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class SecurityController extends Controller
+{
+    /**
+     * @Route("/login", name="security_login")
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function loginAction()
+    {
+
+        return $this->render('security/login.html.twig');
+    }
+
+    /**
+     * @Route("/logout", name="security_logout")
+     * @throws \Exception
+     */
+    public function logoutAction()
+    {
+        throw new \Exception('This should never be reached!');
+    }
+}
