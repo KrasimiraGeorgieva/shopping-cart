1fe57e13f76633c958ee8b19aba56bf037102aa3
Krasimira Georgieva <krassi77bg@gmail.com>
2017-12-13 02:52:17 +0200

    Rewriting the functions.Add info massages. Roles Validations. Security Authenticated.

--- src/ShoppingCartBundle/Controller/UserController.php
+++ src/ShoppingCartBundle/Controller/UserController.php
@@ -4,9 +4,11 @@ namespace ShoppingCartBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use ShoppingCartBundle\Entity\Role;
 use ShoppingCartBundle\Entity\User;
 use ShoppingCartBundle\Form\UserType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\FormError;
 use Symfony\Component\HttpFoundation\Request;
 
@@ -24,21 +26,23 @@ class UserController extends Controller
         $form->handleRequest($request);
 
       if ($form->isSubmitted()&& $form->isValid()) {
+
           if ($user->getPassword() != $user->getConfirm()){
             $form->addError(new FormError("Password mismatch"));
             return $this->render('user/register.html.twig', ['form' => $form->createView()]);
           }
             $password = $this->get('security.password_encoder')->encodePassword($user, $user->getPassword());
             $user->setPassword($password);
-//
-//            $roleRepository = $this->getDoctrine()->getRepository(Role::class);
-//            $userRole = $roleRepository->findOneBy(['name' => 'ROLE_USER']);
-//
-//            $user->addRole($userRole);
+
+            $roleRepository = $this->getDoctrine()->getRepository(Role::class);
+            $userRole = $roleRepository->findOneBy(['name' => 'ROLE_USER']);
+
+            $user->addRole($userRole);
 
             $em = $this->getDoctrine()->getManager();
             $em->persist($user);
             $em->flush();
+
             return $this->redirectToRoute('user_profile');
         }
         return $this->render('user/register.html.twig',['form' => $form->createView()]);
@@ -47,10 +51,34 @@ class UserController extends Controller
     /**
      * @Security("is_granted('IS_AUTHENTICATED_FULLY')")
      * @Route("/profile", name="user_profile")
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
      */
-    public function profileAction()
+    public function profileAction(Request $request)
     {
         $user = $this->getUser();
-        return $this->render("user/profile.html.twig", ['user' => $user]);
+        $form = $this->createForm(UserType::class, $user);
+//        $form->add('wallet', NumberType::class, array(
+//            'data' => $user->getWallet()
+//        ));
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted()&& $form->isValid()) {
+
+            if (($user->getPassword() != $user->getConfirm()) || ($user->getNewPassword() != $user->getConfirm())) {
+                $form->addError(new FormError("Password mismatch"));
+                return $this->render('user/profile.html.twig', ['form' => $form->createView()]);
+            }
+            $password = $this->get('security.password_encoder')->encodePassword($user, $user->getPassword());
+            $user->setPassword($password);
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+
+            $this->addFlash("info", $user->getFullName() . "profile was successfully updated.");
+
+            return $this->redirectToRoute('user_profile');
+        }
+        return $this->render("user/profile.html.twig", ['user' => $user, 'form' => $form->createView()]);
     }
 }
