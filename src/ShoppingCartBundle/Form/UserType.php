2226c37786d57923c5ff10454f74a75540fe9ce9
Krasimira Georgieva <krassi77bg@gmail.com>
2017-12-19 00:16:42 +0200

    Bug fixed in registration form.

--- src/ShoppingCartBundle/Form/UserType.php
+++ src/ShoppingCartBundle/Form/UserType.php
@@ -6,6 +6,7 @@ use ShoppingCartBundle\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -15,12 +16,22 @@ class UserType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+        //$options['data']['username']
+        /**
+         * @var User $user
+         */
+        $user = $options['data'];
+        //dump($user->getFullName()); dump($user->getEmail());
         $builder
-            ->add("email", EmailType::class)
-            ->add("fullName", TextType::class)
+            ->add("email", EmailType::class, array(
+                'data'=> $user->getEmail()
+            ))
+            ->add("fullName", TextType::class, array(
+                'data' => $user->getFullName()
+            ))
             ->add("password", PasswordType::class)
             ->add("confirm", PasswordType::class)
-            ->add("newPassword", PasswordType::class);
+            ->add('submit',SubmitType::class);
     }
 
     public function configureOptions(OptionsResolver $resolver)
