dbb4f8b0891e8779502c7e7478ccf6d08d783aee
Krasimira Georgieva <krassi77bg@gmail.com>
2017-12-22 01:45:30 +0200

    Added BanIpServicesInterface

---
+++ src/ShoppingCartBundle/Services/BanIpServicesInterface.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Krasimira
+ * Date: 12/21/2017
+ * Time: 21:44
+ */
+
+namespace ShoppingCartBundle\Services;
+
+interface BanIpServicesInterface
+{
+    public function findBanIp();
+}
